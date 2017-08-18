e3381d0f650ee4fb3bf43e3f0e0928613d380ff0
Pedro De Ona <pdeon003@fiu.edu>
2017-08-18 01:33:42 -0400

    added code from week 2 hackathon

---
+++ HashTacos.rb
@@ -0,0 +1,23 @@
+require './TaquizaTime.rb'
+​
+taquiza1 = Taquiza.new
+customer1 = Customer.new taquiza1
+​
+# Welcome to Taquiza
+​
+# How can we help you today?
+# place order
+# Start placing your order...
+# espresso
+# done ordering
+# How can we help you today?
+# get the check
+# Thanks for eating at Taquiza. Here's your receipt:
+# espresso: 3
+# Subtotal: 3
+# Tax: 0.21
+# Total: 3.21
+# Recommended Tip:
+#   15%: 3.69
+#   18%: 3.79
+#   20%: 3.85
